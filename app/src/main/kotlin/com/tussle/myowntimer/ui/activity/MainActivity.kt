cfbf69a971967c60e27cb96701f446ded7a0c993
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-07-27 02:44:28 +0900

    init

--- app/src/main/kotlin/com/tussle/myowntimer/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/tussle/myowntimer/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tussle.myowntimer
+package com.tussle.myowntimer.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tussle.myowntimer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
