1b86632e7b7cd63918072413ec656c447d9bfd75
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-07-29 05:17:44 +0900

    Splash Update

--- app/src/main/kotlin/com/tussle/myowntimer/ui/activity/SplashActivity.kt
+++ app/src/main/kotlin/com/tussle/myowntimer/ui/activity/SplashActivity.kt
@@ -1,7 +1,29 @@
 package com.tussle.myowntimer.ui.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.tussle.myowntimer.R
+import com.tussle.myowntimer.databinding.SplashPageBinding
+import com.tussle.myowntimer.event.EventObserver
+import com.tussle.myowntimer.viewmodel.SplashViewModel
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
-
+    private val viewModel : SplashViewModel by lazy {
+        ViewModelProvider(this).get(SplashViewModel::class.java)
+    }
+    private lateinit var binding : SplashPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.splash_page)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        viewModel.splashStart()
+        viewModel.splashEvent.observe(this,EventObserver{
+            startActivity<MainActivity>()
+            finish()
+        })
+    }
 }
--- app/src/main/kotlin/com/tussle/myowntimer/viewmodel/SplashViewModel.kt
+++ app/src/main/kotlin/com/tussle/myowntimer/viewmodel/SplashViewModel.kt
@@ -2,7 +2,24 @@ package com.tussle.myowntimer.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.tussle.myowntimer.event.Event
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashViewModel(application: Application) : AndroidViewModel(application) {
+    private val _splashEvent = MutableLiveData<Event<Boolean>>()
+    val splashEvent : LiveData<Event<Boolean>>
+        get() = _splashEvent
 
+    //Splash Method
+    fun splashStart(){
+        GlobalScope.launch (Dispatchers.IO){
+            delay(3000)
+            _splashEvent.postValue(Event(true))
+        }
+    }
 }
