17da0593d7754e2a0098c7c1350358c840a04da5
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-08-12 04:51:13 +0900

    SettingPage Update

---
+++ app/src/main/kotlin/com/tussle/myowntimer/ui/activity/SettingActivity.kt
@@ -0,0 +1,22 @@
+package com.tussle.myowntimer.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.tussle.myowntimer.R
+import com.tussle.myowntimer.databinding.SettingPageBinding
+import com.tussle.myowntimer.viewmodel.SettingViewModel
+
+class SettingActivity : AppCompatActivity() {
+    private val viewModel : SettingViewModel by lazy {
+        ViewModelProvider(this).get(SettingViewModel::class.java)
+    }
+    private lateinit var binding : SettingPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.setting_page)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+}
---
+++ app/src/main/kotlin/com/tussle/myowntimer/viewmodel/SettingViewModel.kt
@@ -0,0 +1,8 @@
+package com.tussle.myowntimer.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SettingViewModel(application: Application) : AndroidViewModel(application) {
+
+}
